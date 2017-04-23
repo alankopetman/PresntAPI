2f6c532561362cb05028145ff96a98ffe62b4862
Alan Kopetman <alan@presnt-deploy.localdomain>
2017-04-23 00:51:28 +0000

    added deploy settings

--- presnt_api/settings.py
+++ presnt_api/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Django Rest Framework Docs
 SWAGGER_SETTINGS = {
